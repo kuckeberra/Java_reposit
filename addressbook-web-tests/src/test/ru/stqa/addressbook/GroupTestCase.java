6594486cb0ec5a5af8cde2ca2f14f5f515e92232
kuckeberra <ogorodnikovam@yahoo.com>
2020-09-01 21:02:32 +0300

    changed test ok

--- addressbook-web-tests/src/test/ru/stqa/addressbook/GroupTestCase.java
+++ addressbook-web-tests/src/test/ru/stqa/addressbook/GroupTestCase.java
@@ -15,10 +15,6 @@ public class GroupTestCase {
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testGroupCreation() throws Exception {
     wd.get("http://localhost/addressbook/group.php");
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
@@ -26,6 +22,11 @@ public class GroupTestCase {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
+
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).clear();
@@ -45,7 +46,7 @@ public class GroupTestCase {
   public void tearDown() throws Exception {
     wd.quit();
      }
-  }
+
 
   private boolean isElementPresent(By by) {
     try {
@@ -66,4 +67,4 @@ public class GroupTestCase {
   }
 
   }
-}
+
