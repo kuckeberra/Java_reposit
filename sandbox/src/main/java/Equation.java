b4ad3e2f122753fcc0809dfca352fe2c72d730e9
kuckeberra <ogorodnikovam@yahoo.com>
2020-09-14 20:22:33 +0300

    tests equation 3.1 ok

---
+++ sandbox/src/main/java/Equation.java
@@ -0,0 +1,26 @@
+public class Equation {
+  public double a;
+  public double b;
+  public double c;
+  public int n;
+
+  public Equation(double a, double b, double c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+
+    double d = b * b - 4 * a * c;
+    if (d > 0) {
+      n = 2;
+    } else {
+      if (d == 0) {
+        n = 1;
+      } else {
+        n = 0;
+      }
+    }
+  }
+  public int rootNumber(){return n;}
+
+}
+
