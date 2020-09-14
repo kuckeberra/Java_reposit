2093c4ab6c5ce03a9f55d714b287e9b209fa9149
kuckeberra <ogorodnikovam@yahoo.com>
2020-09-14 16:12:23 +0300

    Revert "all exept ContactDeletion"
    This reverts commit a35e707b693096ca5efeb322667cc053c378be5d.

--- addressbook-web-tests/src/test/ru/stqa/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/ru/stqa/addressbook/appmanager/ContactHelper.java
@@ -4,24 +4,33 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import ru.stqa.addressbook.model.ContactData;
 
-public class ContactHelper extends HelperBase {
+public class ContactHelper {
+  public WebDriver wd;
 
   public ContactHelper(WebDriver wd) {
-    super(wd);
+    this.wd=wd;
   }
 
   public void submitContactCreation() {
-    click(By.xpath("(//input[@name='submit'])[2]"));
+    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
   }
 
   public void fillContactForm(ContactData contactData) {
-    type(By.name("firstname"), contactData.getFirstName());
-    type(By.name("lastname"), contactData.getSecondName());
-    type(By.name("mobile"), contactData.getPhone());
-    type(By.name("email"), contactData.getEmail());
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(contactData.getSecondName());
+    wd.findElement(By.name("mobile")).click();
+    wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys(contactData.getPhone());
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
   }
 
   public void addContact() {
-    click(By.linkText("add new"));
+    wd.findElement(By.linkText("add new")).click();
   }
 }
